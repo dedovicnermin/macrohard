fed47c675bd3f53109fe49161b8553e310fa0ef0
dedovicnermin <dedovicnermin@outlook.com>
2020-10-15 00:00:31 -0500

    config/seeder:	added few rows to Reviews config/seederDelete: added all bulk deletes to different file cont/userController: back end functionality to display pie charts and table for users&faculty/admin views/statPage: added test to ensure backend functionality / append charts models/review: two columns have been added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const Review = require('../models/Review');
+
 const express = require('express'),
 userRouter = express.Router(),
 Project = require("../models/Project"),
@@ -11,31 +13,14 @@ User = require("../models/User");
 
 
 
-userRouter.get('/:userId/:projectId/stats', (req, res) => {
-    res.render('statsPage');
+userRouter.get('/:userId/:projectId/stats', async (req, res) => {
+    const obj = await gatherTableInfo(req.params.projectId, req.params.userId);
+    res.render('statsPage', {obj});
 });
 
 
 
-// AJAX REQUESTS
 
-// Route path: /flights/:from/:to
-// Request URL: http://localhost:3000/user/:userId/:projectId/stats/getstats
-// req.params: {"userId" : "", "projectId" : ""}
-
-
-
-
-    
-    
-    
-//});
-// projectStats: {pie1: [], pie2: [], pie3: []},
-// groupStats: {pie1: [], pie2: [], pie3: []},
-// individual: [{username: "", taskName: "", score: 0}]
-
-
-//res.json({label: "label"});
 
 
 
@@ -52,6 +37,94 @@ userRouter.get('/:userId/:projectId/stats/getstats', async (req, res) => {
         res.json({ error: err });
     }
 });
+
+
+
+
+const gatherTableInfo = async (projID, userID) => {
+    try {
+        var isFaculty;
+        const userType = await User.findOne({
+            attributes: ['user_type'],
+            where: {user_id: userID}
+        });
+        
+
+        if (userType.user_type === 'USER') {
+            isFaculty = false;
+        } else {
+            isFaculty = true;
+        }
+
+        var dict = {};
+        var avgs = {};
+        var a = [];
+        if (!isFaculty) {
+            const reviews = await Review.findAll({
+                where: {proj_id: projID, user_id: userID}
+            }).then(allReviews => {
+                allReviews.forEach(review => {
+                    if (dict[review.task_id]) {
+                        dict[review.task_id] = [dict[review.task_id][0] + review.review_score, dict[review.task_id][1] + 1];
+                    } else {
+                        dict[review.task_id] = [review.review_score, 1];
+                    }
+                });
+                
+                for (var key in dict) {
+                    avgs[key] = dict[key][0] / dict[key][1];
+                }
+                console.log(avgs);
+
+                for (var k in avgs) {
+                    a.push({"USER": k, "AVG": avgs[k]});
+                }
+                
+            })
+            .catch(err => console.log(err));
+
+        } else {
+            const reviews = await Review.findAll({
+                where: {proj_id: projID}
+            }).then(allReviews => {
+                allReviews.forEach(review => {
+                    if (dict[[review.task_id, review.user_id]]) {
+                        dict[[review.task_id, review.user_id]] = [dict[[review.task_id, review.user_id]][0] + review.review_score, dict[[review.task_id, review.user_id]][1] + 1];
+                    } else {
+                        dict[[review.task_id, review.user_id]] = [review.review_score, 1];
+                    }
+                });
+
+                for (var key in dict) {
+                    avgs[key] = dict[key][0] / dict[key][1];
+                }
+                
+                for (var k in avgs) {
+                    var s = k.split(",");
+                    a.push({
+                        "TASK": s[0],
+                        "USER": s[1],
+                        "AVG": avgs[k]
+                    });
+                }
+                console.log(a);
+                
+               
+            })
+            .catch(err => console.log(err));
+        }
+
+        return a;
+
+        
+
+
+        
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
 
 
 
@@ -72,6 +145,7 @@ const gatherTaskInfo = async (projID) => {
         const task = async () => {
             await asyncForEach(proj.groups, async (element) => {
                 const t = await Task.findAll({
+                    attributes: ['task_status', 'task_overdue', 'group_id'],
                     where: {
                         group_id: element.group_id
                     }
@@ -83,17 +157,69 @@ const gatherTaskInfo = async (projID) => {
         }
 
         await task();
-
+        tasks = condenseTasks(tasks);
+        var completedOrNot = compNotComp(tasks);
+        var completed = compOnTime(tasks);
+        var notCompleted = notCompOnTime(tasks);
         return {
-            project: proj,
-            groups: proj.groups,
-            tasks: tasks
-        }
+
+            p1: completedOrNot,
+            p2: completed,
+            p3: notCompleted
+            
+        };
 
     } catch (err) {
         console.log(err);
     }
 }
+function compNotComp(tasks) {
+    var comp = 0, notComp = 0;
+    tasks.forEach(task => {
+        if (task.task_status == 'Complete') {
+            comp++;
+        } else {
+            notComp++;
+        }
+    });
+    return [comp, notComp];
+} 
+
+function compOnTime(tasks) {
+    var comp = 0, notComp = 0;
+    tasks.forEach(task => {
+        if (task.task_status == 'Complete' && !task.task_overdue) {
+            comp++;
+        } else if (task.task_status == 'Complete' && task.task_overdue){
+            notComp++;
+        }
+    });
+    return [comp, notComp];
+}
+
+function notCompOnTime(tasks) {
+    var comp = 0, notComp = 0;
+    tasks.forEach(task => {
+        if (task.task_status == 'Not Complete' && !task.task_overdue) {
+            comp++;
+        } else if (task.task_status == 'Not Complete' && task.task_overdue){
+            notComp++;
+        }
+    });
+    return [comp, notComp];
+}
+
+
+
+function condenseTasks(tasks) {
+    var ret = [];
+    tasks.forEach(task => {
+        ret.push(...task);
+    });
+    return ret;
+}
+
+
 
 async function asyncForEach(array, callback) {
     for (let index = 0; index < array.length; index++) {
